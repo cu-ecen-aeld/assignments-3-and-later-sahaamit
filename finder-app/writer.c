fa12e9d32e3fb64c5c0795053af9a3e208bca18b
Amit <amit@gmail.com>
2024-10-01 09:19:58 +0530

    assignment-2 changes

---
+++ finder-app/writer.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <syslog.h>
+
+int main(int argc, char **argv) {
+    printf("Writing %s to %s", argv[1], argv[2]);
+    openlog(NULL, 0, LOG_USER);
+
+    char* content = argv[1];
+    char* filename = argv[2];
+    syslog(LOG_DEBUG, "Writing %s to %s", content, filename);
+
+    FILE *fptr;
+    fptr = fopen(filename, "w");
+    if (fptr == NULL) {
+        syslog(LOG_ERR, "Error to open file pointer %s", filename);
+        return -1;
+    }
+    fprintf(fptr, "%s", content);
+
+    fclose(fptr);
+
+    return 0;
+}
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -12,7 +12,8 @@ path=$(dirname "$1")
 
 mkdir -p "${path}"
 
-echo $2 >> $1
+#echo $2 >> $1
+./writer.o "$2" "$1"
 
 
 
