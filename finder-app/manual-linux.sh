871d6a2543e7d2fdfc8332a11d4a9f5651db59f7
Amit <amit@gmail.com>
2024-10-08 00:56:10 +0530

    Removing TODOs

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -34,7 +34,8 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    # Adding kernel build steps here
+
     # QEMU build - clean: deep clean the kernel build tree - removing the .config file with any existing configuration
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
 
@@ -59,7 +60,7 @@ then
   sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+# Creating necessary base directories
 for dir in bin dev etc home lib lib64 proc sbin sys tmp usr var usr/bin usr/lib usr/sbin var/log; do
   mkdir -p "${OUTDIR}/rootfs/${dir}"
 done
@@ -75,7 +76,7 @@ else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+# Making and installing busybox
 make distclean
 make defconfig
 make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
@@ -85,7 +86,7 @@ echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a "$OUTDIR/rootfs/bin/busybox" | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a "$OUTDIR/rootfs/bin/busybox" | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+# Adding library dependencies to rootfs
 cp "$(dirname $(which ${CROSS_COMPILE}gcc))/../aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1" \
   "$OUTDIR/rootfs/lib/"
 cp "$(dirname $(which ${CROSS_COMPILE}gcc))/../aarch64-none-linux-gnu/libc/lib64/libm.so.6" \
@@ -96,7 +97,7 @@ cp "$(dirname $(which ${CROSS_COMPILE}gcc))/../aarch64-none-linux-gnu/libc/lib64
   "$OUTDIR/rootfs/lib64/"
 
 
-# TODO: Make device nodes
+# Making device nodes
 sudo mknod -m 666 "$OUTDIR/rootfs/dev/null" c 1 3
 sudo mknod -m 666 "$OUTDIR/rootfs/dev/console" c 5 1
 
@@ -104,14 +105,14 @@ sudo mknod -m 666 "$OUTDIR/rootfs/dev/console" c 5 1
 make -C "${FINDER_APP_DIR}/" clean
 make -C "${FINDER_APP_DIR}/"
 
-# TODO: Copy the finder related scripts and executables to the /home directory
+# Copying the finder related scripts and executables to the /home directory
 # on the target rootfs
 cp "${FINDER_APP_DIR}/writer" "$OUTDIR/rootfs/home/"
 
-# TODO: Chown the root directory
+# Changing ownership of root directory
 sudo chown -R root:root "$OUTDIR/rootfs/"
 
-# TODO: Create initramfs.cpio.gz
+# Creating initramfs.cpio.gz
 cd "$OUTDIR/rootfs/"
 find . | cpio -H newC -ov --owner root:root > "${OUTDIR}/initramfs.cpio"
 gzip -f "${OUTDIR}/initramfs.cpio"
