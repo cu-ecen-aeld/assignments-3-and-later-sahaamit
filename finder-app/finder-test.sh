7dd910f7c4dcb0e232e9a24345fbc569511328bb
Amit <amit@gmail.com>
2024-11-02 01:33:03 +0530

    Added changes for assignment-4

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -55,7 +55,7 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	/usr/bin/writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
 OUTPUTSTRING=$(/usr/bin/finder.sh "$WRITEDIR" "$WRITESTR")
